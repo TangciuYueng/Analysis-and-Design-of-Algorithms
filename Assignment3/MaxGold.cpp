e5e64357ec20dacffc1c1854b918a99f8e29b0f6
TangciuYueng <2310360939@qq.com>
2023-05-11 23:13:08 +0800

    update MaxGold

--- Assignment3/MaxGold.cpp
+++ Assignment3/MaxGold.cpp
@@ -7,40 +7,57 @@ int dy[4] = {-1, 1, 0, 0};
 int m, n;
 int ans = 0;
 int temp = 0;
+// 判断是否为合适的起点，周围有两条路以上的处于中间，不适合为起点 
+bool isStart(vector<vector<int>>& grid, int x, int y) {
+    int cnt = 0;
+    for (int i = 0; i < 4; ++i) {
+        int new_x = x + dx[i];
+        int new_y = y + dy[i];
+        if (new_x < 0 || new_y < 0 || new_x >= m || new_y >= n || grid[new_x][new_y] == 0)
+            continue;
+        ++cnt;
+	}
+    return cnt <= 2;
+}
+// 递归函数 
 void help(vector<vector<int>>& grid, vector<vector<bool>>& vis, int x, int y) {
     ans = max(ans, temp);
-        // 出界
-        if (x < 0 || y < 0 || x >= m || y >= n) {
-            return;
-        }
-        // 没东西
-        if (grid[x][y] == 0 || vis[x][y]) {
-            return;
-        }
-        // 当前收获
-        temp += grid[x][y];
-        // 访问过
-        vis[x][y] = true;
-        // 遍历四个方向
-        for (int i = 0; i < 4; ++i) {
-            int new_x = x + dx[i];
-            int new_y = y + dy[i];
-            help(grid, vis, new_x, new_y);
-        }
-        vis[x][y] = false;
-        temp -= grid[x][y];
+    // 出界
+    if (x < 0 || y < 0 || x >= m || y >= n) {
+        return;
     }
-    int getMaximumGold(vector<vector<int>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                vector<vector<bool>> vis(m, vector<bool>(n, false));
-                help(grid, vis, i, j);
-            }
-        }
-        return ans;
+    // 没东西
+    if (grid[x][y] == 0 || vis[x][y]) {
+        return;
     }
+    // 当前收获
+    temp += grid[x][y];
+    // 访问过
+    vis[x][y] = true;
+    // 遍历四个方向
+    for (int i = 0; i < 4; ++i) {
+        int new_x = x + dx[i];
+        int new_y = y + dy[i];
+        help(grid, vis, new_x, new_y);
+	}
+    vis[x][y] = false;
+    temp -= grid[x][y];
+}
+int getMaximumGold(vector<vector<int>>& grid) {
+    m = grid.size();
+    n = grid[0].size();
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+           	// 判断是否为合适的起点 
+           	if (grid[i][j] && isStart(grid, i, j))  {
+        		vector<vector<bool>> vis(m, vector<bool>(n, false));
+               	help(grid, vis, i, j);
+			}
+                
+        }
+    }
+    return ans;
+}
 int main() {
 	int a, b;
 	cout << "rows: ";
