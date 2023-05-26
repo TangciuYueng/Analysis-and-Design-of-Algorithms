20151541cafabf7cc9b6a2e59e1d9fe43e2ffea4
TangciuYueng <2310360939@qq.com>
2023-05-26 21:01:23 +0800

    update problem4

--- Assignment4/code2/grad.py
+++ Assignment4/code2/grad.py
@@ -10,6 +10,7 @@ threshold = 1e-10
 n = 1000
 
 # 函数
+# 返回函数计算结果和求偏导结果
 def fun(x1, x2):
     res = exp(x1 + 3 * x2 - 0.1) + exp(x1 - 3 * x2 - 0.1) + exp(-x1 - 0.1)
     dx1 = exp(x1 + 3 * x2 - 0.1) + exp(x1 - 3 * x2 - 0.1) - exp(-x1 - 0.1)
@@ -20,15 +21,17 @@ def fun(x1, x2):
 if __name__ == '__main__':
     x1, x2 = 0, 0
     y_old, dx1, dx2 = fun(x1, x2)
-
+    # n此迭代
     for i in range(n):
         x1 -= learning_rate * dx1
         x2 -= learning_rate * dx2
-
+        # 得到新计算值
         y_res, dx1, dx2 = fun(x1, x2)
         # print('第%d轮, x1 = %f, x2 = %f' % (i, x1, x2))
-
+        # 变化不大跳出循环
         if abs(y_old - y_res) < threshold:
             break
+        # 更新y_old
+        y_old = y_res
 
     print('x1*= {:.10f} x2*= {:.10f}\nf*= {:.10f}'.format(x1, x2, y_res))
